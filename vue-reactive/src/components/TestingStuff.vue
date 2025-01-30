f903e757fc4e52bd6b0fec30c51d08bd81bed4d5
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2025-01-30 14:03:00 -0500

    saddddddddddddddddddddddddddddd

---
+++ vue-reactive/src/components/TestingStuff.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <button class="bg-black-500">Test</button>
+        <h1>Test 2</h1>
+    </div>
+</template>
+
+<script setup>
+import { reactive } from 'vue';
+</script>
+
+<style lang="css" scoped>
+
+</style>
--- vue-reactive/src/views/AboutView.vue
+++ vue-reactive/src/views/AboutView.vue
@@ -1,15 +1,14 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <TestingStuff></TestingStuff>
+    <h1>Test</h1>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+<script setup>
+import TestingStuff from '../components/TestingStuff.vue'
+</script>
+
+<style lang="css" scoped>
+
 </style>
