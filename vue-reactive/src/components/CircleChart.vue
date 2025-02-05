0152083e7713b644f300c731e87c788b0c8c626e
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2025-02-05 14:02:53 -0500

    xzczxczxcxz

--- vue-reactive/src/components/CircleChart.vue
+++ vue-reactive/src/components/CircleChart.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="bg-white rounded-[50%] w-[45vw] h-[45vw] m-6">
+    <div class="bg-white rounded-[50%] w-[45vw] h-[45vw] m-6 flex justify-evenly items-center flex-wrap">
+        <div class="bg-pink-400 rounded-[50%] w-[15vw] h-[15vw] absolute border-4 border-pink-600"><img class="object-fill" src="../images/spinning.png" alt="CircleCenter"></div>
         <CircleSection></CircleSection>
     </div>
 </template>
--- vue-reactive/src/components/CircleSection.vue
+++ vue-reactive/src/components/CircleSection.vue
@@ -1,5 +1,8 @@
 <template>
-    <button class="bg-black">Test</button>
+    <div class="w-1/2 h-1/2 bg-cyan-600 border-white border-t-32 border-l-32 border-b-16 border-r-16 rounded-tl-[100%] hover:bg-black hover:scale-300"></div>
+    <div class="w-1/2 h-1/2 bg-cyan-600 border-white border-t-32 border-l-16 border-b-16 border-r-32 rounded-tr-[100%]"></div>
+    <div class="w-1/2 h-1/2 bg-cyan-600 border-white border-t-16 border-l-32 border-b-32 border-r-16 rounded-bl-[100%]"></div>
+    <div class="w-1/2 h-1/2 bg-cyan-600 border-white border-t-16 border-l-16 border-b-32 border-r-32 rounded-br-[100%]"></div>
 </template>
 
 <script setup>
