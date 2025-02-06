00188b40eda00e25c8e3255bb6c38195305c4a93
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2025-02-06 13:55:17 -0500

    sad

--- vue-reactive/src/components/CircleChart.vue
+++ vue-reactive/src/components/CircleChart.vue
@@ -1,14 +1,26 @@
 <template>
-    <div class="bg-white rounded-[50%] w-[45vw] h-[45vw] m-6 flex justify-evenly items-center flex-wrap">
-        <div class="bg-pink-400 rounded-[50%] w-[15vw] h-[15vw] absolute border-4 border-pink-600"><img class="object-fill" src="../images/spinning.png" alt="CircleCenter"></div>
+    <div class="bg-sky-100 rounded-[50%] w-[40vw] h-[40vw] m-6 flex justify-evenly items-center flex-wrap">
+        <div class="bg-rose-300 border-18 border-sky-100 rounded-[50%] w-[14vw] h-[14vw] absolute"><img class="spinner object-fill" src="../images/spinning.png" alt="CircleCenter"></div>
         <CircleSection></CircleSection>
     </div>
 </template>
 
 <script setup>
 import CircleSection from './CircleSection.vue';
+
+
 </script>
 
 <style lang="css" scoped>
+
+.spinner{
+    animation: 20s linear infinite spin;
+}
+
+@keyframes spin {
+  to {
+    transform: rotate(360deg);
+  }
+}
 
 </style>
--- vue-reactive/src/components/CircleSection.vue
+++ vue-reactive/src/components/CircleSection.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="w-1/2 h-1/2 bg-cyan-600 border-white border-t-32 border-l-32 border-b-16 border-r-16 rounded-tl-[100%] hover:bg-black hover:scale-300"></div>
-    <div class="w-1/2 h-1/2 bg-cyan-600 border-white border-t-32 border-l-16 border-b-16 border-r-32 rounded-tr-[100%]"></div>
-    <div class="w-1/2 h-1/2 bg-cyan-600 border-white border-t-16 border-l-32 border-b-32 border-r-16 rounded-bl-[100%]"></div>
-    <div class="w-1/2 h-1/2 bg-cyan-600 border-white border-t-16 border-l-16 border-b-32 border-r-32 rounded-br-[100%]"></div>
+    <div class="w-1/2 h-1/2 bg-fuchsia-500 border-sky-100 border-t-[1.5vw] border-l-[1.5vw] border-b-[0.5vw] border-r-[0.5vw] rounded-tl-[100%] hover:bg-black hover:scale-300"></div>
+    <div class="w-1/2 h-1/2 bg-blue-500 border-sky-100 border-t-[1.5vw] border-l-[0.5vw] border-b-[0.5vw] border-r-[1.5vw] rounded-tr-[100%]"></div>
+    <div class="w-1/2 h-1/2 bg-yellow-400 border-sky-100 border-t-[0.5vw] border-l-[1.5vw] border-b-[1.5vw] border-r-[0.5vw] rounded-bl-[100%]"></div>
+    <div class="w-1/2 h-1/2 bg-emerald-500 border-sky-100 border-t-[0.5vw] border-l-[0.5vw] border-b-[1.5vw] border-r-[1.5vw] rounded-br-[100%]"></div>
 </template>
 
 <script setup>
