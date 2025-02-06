5e5e68290c22394e6ec0e78ef2c7f37ac2f6f3e2
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2025-02-06 14:03:37 -0500

    sadsa

--- vue-reactive/src/components/CircleChart.vue
+++ vue-reactive/src/components/CircleChart.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="bg-sky-100 rounded-[50%] w-[40vw] h-[40vw] m-6 flex justify-evenly items-center flex-wrap">
-        <div class="bg-rose-300 border-18 border-sky-100 rounded-[50%] w-[14vw] h-[14vw] absolute"><img class="spinner object-fill" src="../images/spinning.png" alt="CircleCenter"></div>
+        <div class="bg-linear-65 from-purple-700 via-pink-400 to-orange-300 border-18 border-sky-100 rounded-[50%] w-[14vw] h-[14vw] absolute"><img class="spinner object-fill" src="../images/spinning.png" alt="CircleCenter"></div>
         <CircleSection></CircleSection>
     </div>
 </template>
--- vue-reactive/src/components/CircleSection.vue
+++ vue-reactive/src/components/CircleSection.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="w-1/2 h-1/2 bg-fuchsia-500 border-sky-100 border-t-[1.5vw] border-l-[1.5vw] border-b-[0.5vw] border-r-[0.5vw] rounded-tl-[100%] hover:bg-black hover:scale-300"></div>
-    <div class="w-1/2 h-1/2 bg-blue-500 border-sky-100 border-t-[1.5vw] border-l-[0.5vw] border-b-[0.5vw] border-r-[1.5vw] rounded-tr-[100%]"></div>
-    <div class="w-1/2 h-1/2 bg-yellow-400 border-sky-100 border-t-[0.5vw] border-l-[1.5vw] border-b-[1.5vw] border-r-[0.5vw] rounded-bl-[100%]"></div>
-    <div class="w-1/2 h-1/2 bg-emerald-500 border-sky-100 border-t-[0.5vw] border-l-[0.5vw] border-b-[1.5vw] border-r-[1.5vw] rounded-br-[100%]"></div>
+    <div class="w-1/2 h-1/2 bg-linear-65 from-purple-600 to-fuchsia-500 border-sky-100 border-t-[1.5vw] border-l-[1.5vw] border-b-[0.5vw] border-r-[0.5vw] rounded-tl-[100%] hover:bg-black hover:scale-300"></div>
+    <div class="w-1/2 h-1/2 bg-linear-65 from-blue-600 to-sky-500 border-sky-100 border-t-[1.5vw] border-l-[0.5vw] border-b-[0.5vw] border-r-[1.5vw] rounded-tr-[100%]"></div>
+    <div class="w-1/2 h-1/2 bg-linear-65 from-orange-400 to-yellow-400 border-sky-100 border-t-[0.5vw] border-l-[1.5vw] border-b-[1.5vw] border-r-[0.5vw] rounded-bl-[100%]"></div>
+    <div class="w-1/2 h-1/2 bg-linear-65 from-green-600 to-emerald-500 border-sky-100 border-t-[0.5vw] border-l-[0.5vw] border-b-[1.5vw] border-r-[1.5vw] rounded-br-[100%]"></div>
 </template>
 
 <script setup>
