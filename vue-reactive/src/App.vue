410411cf4ee00d023e866591f94afca16a9b59d7
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2025-02-10 10:17:13 -0500

    sacx

--- vue-reactive/src/App.vue
+++ vue-reactive/src/App.vue
@@ -4,12 +4,16 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <div class="page">
-    <div class="flex bg-slate-800 h-[4vw] items-center">
-      <RouterLink class="text-[2vw] text-sky-500 px-[3vw] font-serif underline" to="/">Home</RouterLink>
-      <RouterLink class="text-[2vw] text-sky-500 px-[3vw] font-serif underline" to="/profile">Profile</RouterLink>
+    <div class="header flex h-[10vh] items-center">
+      <RouterLink class="text-[2vw] text-emerald-600 rounded-lg bg-white mx-[2vw] px-[4vw] font-serif font-semibold underline" to="/">Home</RouterLink>
+      <RouterLink class="text-[2vw] text-emerald-600 rounded-lg bg-white mx-[2vw] px-[4vw] font-serif font-semibold underline" to="/profile">Profile</RouterLink>
     </div>
-
-    <RouterView/>
+    <div class="interactive flex h-[90vh] justify-evenly items-center">
+      <RouterView/>
+      <div class="infobox w-[28vw] h-5/6 bg-teal-100 rounded-xl border-6">
+        <h1 class="text"></h1>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,8 +21,18 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <style lang="css" scoped>
 .page{
-  background-color: rgb(40, 62, 87);
+  background-color: rgb(255, 255, 255);
   width: 100vw;
   height: 100vh;
+}
+.header{
+  background-color: rgb(0, 146, 146);
+}
+.infobox{
+  border-color: rgb(0, 146, 146);
+  overflow-y: scroll;
+}
+.text{
+  font-size: 32;
 }
 </style>
