772dfb8770f67f557f4a9755ca903d66b49dd902
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2025-02-11 09:41:11 -0500

    scfe

--- vue-reactive/src/components/TestComponent.vue
+++ vue-reactive/src/components/TestComponent.vue
@@ -1,9 +1,11 @@
 <template>
-    <div class="bg-black w-[45vw]" v-for="(sect) in circleSects"><h1>{{ angle, initColor }}</h1></div>
+    <div class="w-1/2 h-1/2 bg-linear-{{sect.angle}} from-{{sect.initColor}} to-{{sect.finalColor}} border-sky-100 border-t-[{{sect.top}}] border-l-[{{sect.left}}] border-b-[{{sect.bottom}}] border-r-[{{sect.right}}] rounded-br-[100%]"></div>
 </template>
 
 <script setup>
-circleSects = [{angle: 135, initColor: "purple-600", finalColor: "fuchsia-500", top: "1.5vw", left: "1.5vw", bottom: "0.5vw", right: "0.5vw"}, {angle: 135, initColor: "purple-600", finalColor: "fuchsia-500", top: "1.5vw", left: "1.5vw", bottom: "0.5vw", right: "0.5vw"}]
+defineProps({
+    sect: Object
+})
 </script>
 
 <style lang="css" scoped>
--- vue-reactive/src/views/ProfilePage.vue
+++ vue-reactive/src/views/ProfilePage.vue
@@ -1,12 +1,54 @@
 <template>
-    <div>
-        <h1>Text</h1>
-        <h1 v-for="sect in circleSects" :key="sect.angle">{{ sect.angle }}</h1>
+    <div class="circle bg-sky-100 rounded-[50%] w-[40vw] h-[40vw] m-6 flex justify-evenly items-center flex-wrap">
+        <button class="spin-button hover:cursor-pointer bg-linear-65 from-purple-700 via-pink-400 to-orange-300 border-18 border-sky-100 rounded-[50%] w-[14vw] h-[14vw] absolute"><img class="spinner object-fill" src="../images/spinning.png" alt="CircleCenter"></button>
+        <TestComponent v-for="(sect, index) in circleSects" :key="index" :sect="sect"></TestComponent>
+        <li v-for="(sect, index) in circleSects">{{ sect.angle }}</li>
     </div>
 </template>
 
 <script setup>
-circleSects = [{angle: 135, initColor: "purple-600", finalColor: "fuchsia-500", top: "1.5vw", left: "1.5vw", bottom: "0.5vw", right: "0.5vw"}, {angle: 135, initColor: "purple-600", finalColor: "fuchsia-500", top: "1.5vw", left: "1.5vw", bottom: "0.5vw", right: "0.5vw"}]
+import TestComponent from '../components/TestComponent.vue';
+
+
+const circleSects = [
+    {
+        angle: 135, 
+        initColor: "purple-600", 
+        finalColor: "fuchsia-500", 
+        top: "1.5vw", 
+        left: "1.5vw", 
+        bottom: "0.5vw", 
+        right: "0.5vw"
+    }, 
+    {
+        angle: 135, 
+        initColor: "purple-600", 
+        finalColor: "fuchsia-500", 
+        top: "1.5vw", 
+        left: "1.5vw", 
+        bottom: "0.5vw", 
+        right: "0.5vw"
+    },
+    {
+        angle: 135, 
+        initColor: "purple-600", 
+        finalColor: "fuchsia-500", 
+        top: "1.5vw", 
+        left: "1.5vw", 
+        bottom: "0.5vw", 
+        right: "0.5vw"
+    },
+    {
+        angle: 135, 
+        initColor: "purple-600", 
+        finalColor: "fuchsia-500", 
+        top: "1.5vw", 
+        left: "1.5vw", 
+        bottom: "0.5vw", 
+        right: "0.5vw"
+    },
+]
+
 </script>
 
 <style lang="css" scoped>
