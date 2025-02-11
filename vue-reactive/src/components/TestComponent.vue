cf5f75c0a9f4c03aa4bb03f8d0bc6d5abebe9d0a
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2025-02-11 09:51:02 -0500

    qwda

--- vue-reactive/src/components/TestComponent.vue
+++ vue-reactive/src/components/TestComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-1/2 h-1/2 bg-linear-{{sect.angle}} from-{{sect.initColor}} to-{{sect.finalColor}} border-sky-100 border-t-[{{sect.top}}] border-l-[{{sect.left}}] border-b-[{{sect.bottom}}] border-r-[{{sect.right}}] rounded-br-[100%]"></div>
+    
 </template>
 
 <script setup>
