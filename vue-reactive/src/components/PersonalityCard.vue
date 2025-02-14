0653612587d27da5153d9c5c6151795cee8d3264
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2025-02-14 14:12:00 -0500

    sdadadsaXZCFsthxnh bfgzsrdq
    aszdF

--- vue-reactive/src/components/PersonalityCard.vue
+++ vue-reactive/src/components/PersonalityCard.vue
@@ -1,11 +1,19 @@
 <template>
-    <div>
-
+    <div class="">
+        <h1>{{title}}, {{label}}, {{image}}</h1>
     </div>
 </template>
 
 <script setup>
+import {sectCirc} from "./CircleChart.vue"
 
+defineProps({
+    title: String,
+    label: String,
+    image: String,
+})
+
+console.log(sectCirc[1])
 </script>
 
 <style lang="css" scoped>
--- vue-reactive/src/components/SectCards.vue
+++ vue-reactive/src/components/SectCards.vue
@@ -1,6 +1,11 @@
 
 <template>
-    <PersonalityCard v-for="card in cards" :key="title" :card="card"/>
+    <PersonalityCard 
+        v-for="card in cards" 
+        :key="card.title" 
+        :title="card.title"
+        :label="card.label"
+        :image="card.image"/>
 </template>
 
 <script setup>
@@ -12,22 +17,22 @@ const route = useRoute()
 const cards = reactive([]);
 
 const cardList = [
-    { title: 'ISFJ-NTXLOIDKWHATIMDOINGYEAHKDS', name:"The Placheolder", type: "analysts",  image: 'sometype of url/link coming from images folder' },
-    { name: "analysts", title: 'Card 2', description: 'This is card number 2' },
-    { name: "analysts", title: 'Card 3', description: 'This is card number 3' },
-    { name: "analysts", title: 'Card 4', description: 'This is card number 4' },
-    { name: "analysts", title: 'Card 5', description: 'This is card number 5' },
-    { name: "analysts", title: 'Card 6', description: 'This is card number 6' },
-    { name: "analysts", title: 'Card 7', description: 'This is card number 7' },
-    { name: "random", title: 'Card 8', description: 'This is card number 8' },
-    { name: "analysts", title: 'Card 9', description: 'This is card number 9' },
-    { name: "analysts", title: 'Card 10', description: 'This is card number 10' }
+    { title: 'ISFJ-NTXLOIDKWHATIMDOINGYEAHKDS', label:"The Placheolder", image: 'sometype of url/link coming from images folder', type: "analysts"},
+    { label: "analysts", title: 'Card 2', description: 'This is card number 2' },
+    { label: "analysts", title: 'Card 3', description: 'This is card number 3' },
+    { label: "analysts", title: 'Card 4', description: 'This is card number 4' },
+    { label: "analysts", title: 'Card 5', description: 'This is card number 5' },
+    { label: "analysts", title: 'Card 6', description: 'This is card number 6' },
+    { label: "analysts", title: 'Card 7', description: 'This is card number 7' },
+    { label: "random", title: 'Card 8', description: 'This is card number 8' },
+    { label: "analysts", title: 'Card 9', description: 'This is card number 9' },
+    { label: "analysts", title: 'Card 10', description: 'This is card number 10' }
 ];
 
 onMounted(() => {
-    const cardName = route.params.name;
+    const cardType = route.params.name;
     cardList
-        .filter(item => item.type === cardName)
+        .filter(item => item.type === cardType)
         .forEach(item => cards.push(item))
 });
 </script>
